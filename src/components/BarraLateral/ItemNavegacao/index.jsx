d164118658d8689e782015104a0eaa5da32d6317
Raí <megac154@gmail.com>
2023-09-01 15:53:06 -0300

    style ItemNavegacao and fonts

--- src/components/BarraLateral/ItemNavegacao/index.jsx
+++ src/components/BarraLateral/ItemNavegacao/index.jsx
@@ -1,12 +1,20 @@
 import { styled } from "styled-components"
 
 const ItemListaEstilizado = styled.li`
-  
+  font-size: 24px;
+  line-height: 29px;
+  margin-bottom: 30px;
+  cursor: pointer;
+  color: ${ props => props.$ativo ? '#7B78E5' : '#D9D9D9' };
+  display: flex;
+  align-items: center;
+  gap: 22px;
 `
 
 export default function ItemNavegacao({ children, iconeAtivo, iconeInativo, ativo = false }) {
   return (
-    <ItemListaEstilizado>
+    // passando prop pro styledList
+    <ItemListaEstilizado $ativo={ativo}>
       <img src={ativo ? iconeAtivo : iconeInativo} alt="" />
       {children}
     </ItemListaEstilizado>
